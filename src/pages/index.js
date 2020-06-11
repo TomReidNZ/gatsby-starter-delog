16b42314bd6514aa43dcb42fe9f1b92e37004be4
tomreidnz <treid.nz@gmail.com>
2020-06-12 01:14:46 +1200

    almost final test

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,17 +34,22 @@ class Index extends React.Component {
         </div>
         <div className="section section-2">
           <div className="content-wrapper">
-          <div className="purple-small-title">
-            <h5>BLEEDING-EDGE EXPERTISE</h5>
+            <div className="purple-small-title">
+              <h5>BLEEDING-EDGE EXPERTISE</h5>
+            </div>
+            <div className="section-content">
+              You need a partner that's with you on the cutting edge, who
+              understands what you're working on, and who can translate that
+              knowledge for any audience - from the C-suite down.
+            </div>
           </div>
-          <div className="section-content">
-            You need a partner that's with you on the cutting edge, who
-            understands what you're working on, and who can translate that
-            knowledge for any audience - from the C-suite down.
-          </div>
-          </div>
+          <div className="image-section-center">
           <div className="image-section-wrapper">
-            <img className="section-2-image" src={"./images/section-2-image.jpg"}/>
+            <img
+              className="section-2-image"
+              src={"./images/section-2-image.png"}
+            />
+          </div>
           </div>
         </div>
         <div className="section purple-section section-3">
@@ -53,88 +58,97 @@ class Index extends React.Component {
             <div className="line" />
           </div>
           <div className="services-list">
-            <div className="services-list-left">
-              <div className="item-wrapper">
-                <div className="image-wrapper">
-                  <img
-                    className="services-vector"
-                    src={"./images/ai_and_ml.svg"}
-                  />
-                </div>
-                <h5 className="service-title-left">AI AND ML</h5>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img
+                  className="services-vector"
+                  src={"./images/ai_and_ml.svg"}
+                />
+              </div>
+              <div className="service-title">
+                <h5>AI AND ML</h5>
               </div>
             </div>
-            <div className="services-list-items">
-              <div className="item-wrapper">
-                <div className="image-wrapper">
-                  <img
-                    className="services-vector"
-                    src={"./images/online_education.svg"}
-                  />
-                </div>
-                <h5 className="service-title">ONLINE EDUCATION</h5>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img
+                  className="services-vector"
+                  src={"./images/online_education.svg"}
+                />
+              </div>
+              <div className="service-title">
+                <h5>ONLINE EDUCATION</h5>
               </div>
             </div>
-            <div className="services-list-right">
-              <div className="item-wrapper">
-                <div className="image-wrapper">
-                  <img
-                    className="services-vector"
-                    src={"./images/responsible_ai.svg"}
-                  />
-                </div>
-                <h5 className="service-title">RESPONSIBLE AI</h5>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img
+                  className="services-vector"
+                  src={"./images/responsible_ai.svg"}
+                />
+              </div>
+              <div className="service-title">
+                <h5>RESPONSIBLE AI</h5>
               </div>
             </div>
-            <div className="services-list-left">
-                <div className="image-wrapper">
-                  <img className="services-vector" src={"./images/b.svg"} />
-                </div>
-                <h5 className="service-title-left">HEALTHCARE</h5>
-            </div>
-            <div className="services-list-items">
-              <div className="item-wrapper">
-                <div className="image-wrapper">
-                  <img
-                    className="services-vector"
-                    src={"./images/strategy.svg"}
-                  />
-                </div>
-                <h5 className="service-title">STRATEGY</h5>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img className="services-vector" src={"./images/b.svg"} />
+              </div>
+              <div className="service-title">
+                <h5>HEALTHCARE</h5>
               </div>
             </div>
-            <div className="services-list-right">
-              <div className="item-wrapper">
-                <div className="image-wrapper">
-                  <img
-                    className="services-vector"
-                    src={"./images/software.svg"}
-                  />
-                </div>
-                <h5 className="service-title">SOFTWARE</h5>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img
+                  className="services-vector"
+                  src={"./images/strategy.svg"}
+                />
+              </div>
+              <div className="service-title">
+                <h5>STRATEGY</h5>
+              </div>
+            </div>
+            <div className="item-wrapper">
+              <div className="image-wrapper">
+                <img
+                  className="services-vector"
+                  src={"./images/software.svg"}
+                />
+              </div>
+              <div className="service-title">
+                <h5>SOFTWARE</h5>
               </div>
             </div>
           </div>
         </div>
-        <div className="section section-2">
-          <div className="purple-small-title">
-            <h5>A FULL SERVICE AGENCY</h5>
+        <div className="section section-4">
+          <div className="lady-image-wrapper"><img className="lady-image" src={"./images/lady.jpg"}/></div>
+          <div className="purple-container">
+            <div className="purple-small-title">
+              <h5>A FULL SERVICE AGENCY</h5>
+            </div>
+            <div className="section-content">
+              You need a partner that can deliver projects end-to-end? That's
+              us.
+              <br></br>
+              <br></br>
+              We can cover every part of the process, saving you from dealing
+              with too many vendors, and most importantly, delivering a polished
+              project on time.
+            </div>
           </div>
-          <div className="section-content">
-            You need a partner that can deliver projects end-to-end? That's us.
-          <br></br>
-          <br></br>
-          We can cover every part of the process, saving you from dealing with too many vendors, and most importantly, delivering a polished project on time.</div>
+        </div>
+        <div className="section purple-section section-5">
+          <div className="purple-section-content">
+            Coding samples.<br></br>
+            Curriculum building.<br></br>
+            Medical imaging.<br></br>
+            Everything data.
+            <div className="line-bottom" />
           </div>
-        <div className="section purple-section">
-        <div className="purple-section-content">
-          Coding samples.<br></br>
-          Curriculum building.<br></br>
-          Medical imaging.<br></br>
-          Everything data.
-          <div className="line-bottom" />
-          </div>
-          </div>
+        </div>
         {/* </Container> */}
       </Layout>
     );
